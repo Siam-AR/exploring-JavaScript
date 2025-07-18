a55fcad8fbda140c60b8806966ec44ba05b5f261
Siam-AR <siam.ar.nexus@gmail.com>
2025-07-18 03:25:01 +0000

    JavaScript User Input on Node.js

---
+++ JavaScript_UserInput/02_inputPromptSync.js
@@ -0,0 +1,8 @@
+// Before running this code, install prompt-sync using:
+// npm install prompt-sync
+
+const prompt = require("prompt-sync")(); 
+
+const name = prompt("Enter your name: "); 
+
+console.log(`Hello, ${name}!`);
